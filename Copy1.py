08ef143b6ac434e3d2e4dae864df383227686945
Nikhil Nalluri <nikhilnalluri@Nikhils-MacBook-Pro.local>
2023-10-30 16:37:59 -0400

    Update2

--- Copy1.py
+++ Copy1.py
@@ -1,9 +1,25 @@
-a =4
-b =4
-c =7
-sum = a + b * c;
-print(sum)
+# Python 3.x code to demonstrate star pattern
 
+# Function to demonstrate printing pattern
+def pypart(n):
+	
+	# outer loop to handle number of rows
+	# n in this case
+	for i in range(0, n):
+	
+		# inner loop to handle number of columns
+		# values changing acc. to outer loop
+		for j in range(0, i+1):
+		
+			# printing stars
+			print("* ",end="")
+	
+		# ending line after each row
+		print("\r")
+
+# Driver Code
+n = 5
+pypart(n)
 
 
 
